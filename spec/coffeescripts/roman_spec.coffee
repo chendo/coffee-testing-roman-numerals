1a95e0c01e0e92de10d838a24f7d63329a8c88ef
Jack Chen (chendo) <me@chen.do>
2011-07-22 15:54:25 +1000

    Added a lot of tests and fixed 9 -> IX

--- spec/coffeescripts/roman_spec.coffee
+++ spec/coffeescripts/roman_spec.coffee
@@ -13,8 +13,17 @@ describe 'toRoman()', ->
     6: 'VI'
     7: 'VII'
     8: 'VIII'
-    9: 'XI'
+    9: 'IX'
     10: 'X'
+    11: 'XI'
+    12: 'XII'
+    13: 'XIII'
+    14: 'XIV'
+    15: 'XV'
+    16: 'XVI'
+    17: 'XVII'
+    18: 'XVIII'
+    19: 'XIX'
 
   for integer, roman of tests
     testRoman integer, roman
--- spec/javascripts/roman_spec.js
+++ spec/javascripts/roman_spec.js
@@ -15,8 +15,17 @@
       6: 'VI',
       7: 'VII',
       8: 'VIII',
-      9: 'XI',
-      10: 'X'
+      9: 'IX',
+      10: 'X',
+      11: 'XI',
+      12: 'XII',
+      13: 'XIII',
+      14: 'XIV',
+      15: 'XV',
+      16: 'XVI',
+      17: 'XVII',
+      18: 'XVIII',
+      19: 'XIX'
     };
     _results = [];
     for (integer in tests) {
